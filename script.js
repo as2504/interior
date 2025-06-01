51782abcf8ffa467fbd30a11530257d6220a58fd
as2504 <sumitskmh24@gmail.com>
2025-06-01 18:19:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 function sendMail(){
-    let parms = {
+    const parms = {
         name: document.getElementById("name").value,
         email: document.getElementById("email").value,
         phone: document.getElementById("phone").value,
         service: document.getElementById("service").value,
         message: document.getElementById("message").value,
-    }
-    emailjs.send("service_4x8h8sm","template_c0qf59z", parms).then(alert("Message Sent!"))
+    };
+    emailjs
+        .send("service_4x8h8sm","template_c0qf59z", parms)
+        .then(() => alert("Message Sent!").catch(() => alert("Message not sent!")));
 }
 
