3c10c1eada4a244a11eb62b7d2ab76e8b0bd53e2
as2504 <sumitskmh24@gmail.com>
2025-06-01 17:38:35 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function sendMain(){
+    let parms = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        phone: document.getElementById("phone").value,
+        service: document.getElementById("service").value,
+        message: document.getElementById("message").value,
+    }
+    emailjs.send("service_4x8h8sm","template_c0qf59z", parms).then(alert("Message Sent!"))
+}
